60943cb09897235b283237d2fa0607c99854215c
이자훈 <leejahun0@gmail.com>
2020-04-29 17:41:08 +0900

    Update hook.ts

--- pages/api/hook.ts
+++ pages/api/hook.ts
@@ -1,5 +1,28 @@
-export default (req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'application/json')
-    res.end(JSON.stringify({ name: 'John Doe' }))
+import axios from "axios";
+
+const url = 'https://hooks.slack.com/services/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';
+const headers = {
+    "Content-Type": "application/json"
+};
+const body = {
+    text: 'master에 배포합니다.'
+};
+
+export default async(req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    try {
+        await doHook();
+    } catch (error) {
+        res.end(JSON.stringify({ result: error }));
+    }
+    res.end(JSON.stringify({ result: 'SUCCESS' }));
+}
+
+async function doHook() {
+    try {
+        console.log('Web Hook 실행');
+        await axios.post( url, body, { headers });
+    } catch (error) {
+        console.log('error', error);
+    }
 }
