a96a19fe3e58c012838ac5fe23439094beb28713
이자훈 <leejahun0@gmail.com>
2020-04-29 18:25:06 +0900

    hook 수정

--- pages/api/hook.ts
+++ pages/api/hook.ts
@@ -1,21 +1,26 @@
 import axios from "axios";
+import { IncomingWebhook } from "@slack/webhook";
 
-const url = 'https://hooks.slack.com/services/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';
+const url = 'https://hooks.slack.com/services/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';
 const headers = {
     "Content-Type": "application/json"
 };
 const body = {
     text: 'master에 배포합니다.'
 };
+const webhook = new IncomingWebhook(url);
 
 export default async(req, res) => {
     res.setHeader('Content-Type', 'application/json');
+    let result = 'SUCCESS';
     try {
-        await doHook();
+        await webhook.send({
+            text: 'master에 배포합니다.'
+        });
     } catch (error) {
-        res.end(JSON.stringify({ result: error }));
+        result = error;
     }
-    res.end(JSON.stringify({ result: 'SUCCESS' }));
+    res.end(JSON.stringify({ result }));
 }
 
 async function doHook() {
