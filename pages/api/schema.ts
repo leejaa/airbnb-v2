f0b87b62e7a7b19f8981f68bff9d95b93cb4e7da
이자훈 <leejahun0@gmail.com>
2020-05-02 14:40:25 +0900

    스키마 수정

--- pages/api/schema.ts
+++ pages/api/schema.ts
@@ -1,6 +1,7 @@
-import { makeSchema, objectType, stringArg, asNexusMethod } from 'nexus'
+import { makeSchema, objectType, stringArg, asNexusMethod, intArg } from 'nexus';
 import { GraphQLDate } from 'graphql-iso-date';
 import { PrismaClient } from '@prisma/client';
+import { hash, compare } from "bcryptjs";
 
 export const GQLDate = asNexusMethod(GraphQLDate, 'date')
 
@@ -13,7 +14,19 @@ const Photo = objectType({
     t.string('caption')
     t.string('file')
   },
-})
+});
+
+const User = objectType({
+  name: 'User',
+  definition(t) {
+    t.int('id')
+    t.string('name')
+    t.string('email')
+    t.string('password')
+  }
+});
+
+const Objects = [ Photo, User ];
 
 const Query = objectType({
   name: 'Query',
@@ -27,37 +40,45 @@ const Query = objectType({
         })
       },
     })
+    t.list.field('selectUser', {
+      type: 'User',
+      args: { id: intArg({ required: true }) },
+      resolve: async(_parent, { id }, ctx) => {
+        const user = await prisma.user.findMany({
+          where: { id }
+        });
+        return user;
+      },
+    })
   },
 })
 
 const Mutation = objectType({
   name: 'Mutation',
   definition(t) {
-    // t.field('createDraft', {
-    //   type: 'Post',
-    //   args: {
-    //     title: stringArg({ nullable: false }),
-    //     content: stringArg(),
-    //     authorEmail: stringArg(),
-    //   },
-    //   resolve: (_, { title, content, authorEmail }, ctx) => {
-    //     return prisma.post.create({
-    //       data: {
-    //         title,
-    //         content,
-    //         published: false,
-    //         author: {
-    //           connect: { email: authorEmail },
-    //         },
-    //       },
-    //     })
-    //   },
-    // })
+    t.field('createUser', {
+      type: 'User',
+      args: {
+        email: stringArg({ nullable: false }),
+        name: stringArg(),
+        password: stringArg(),
+      },
+      resolve: async(_, { email, name, password }, ctx) => {
+        const hashedPassword = await hash(password, 12);
+        return prisma.user.create({
+          data: {
+            email,
+            name,
+            password: hashedPassword
+          },
+        })
+      },
+    })
   },
 })
 
 export const schema = makeSchema({
-  types: [Query, Photo, GQLDate],
+  types: [Query, Mutation, Objects, GQLDate],
   outputs: {
     // typegen: path.join(__dirname, 'nexus-typegen.ts'),
     // schema: path.join(__dirname, 'schema.graphql')
