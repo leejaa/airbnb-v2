94a8b3c047485abe211b03699799b8594f469fcd
이자훈 <leejahun0@gmail.com>
2020-06-18 12:56:01 +0900

    Update Calendar.tsx

--- app/components/Common/Calendar.tsx
+++ app/components/Common/Calendar.tsx
@@ -33,10 +33,50 @@ const Container3: any = styled.View`
 const Container4: any = styled.ScrollView`
     width: 100%;
     height: 93%;
-    border-width: 1px;
-    border-color: black;
+`;
+const Container5: any = styled.View`
+    width: 100%;
+    height: ${SCREEN_HEIGHT / 2.5}px;
+`;
+const Container6: any = styled.View`
+    width: 100%;
+    height: 15%;
+    display: flex;
+    justify-content: center;
+    padding-left: ${SCREEN_WIDTH / 20}px;
+`;
+const Container7: any = styled.View`
+    width: 100%;
+    height: 13%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+`;
+const Container8: any = styled.View`
+    width: 14.2857%;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+`;
+const Container9: any = styled.View`
+    width: 80%;
+    height: 100%;
+    border-radius: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `;
 const Text1: any = styled.Text`
+`;
+const Text2: any = styled.Text`
+    font-size: ${SCREEN_WIDTH / 22}px;
+    font-weight: 600;
+`;
+const Text3: any = styled.Text`
+    font-size: ${SCREEN_WIDTH / 25}px;
+    font-weight: 600;
+    color: ${(props: any) => moment(props.date).isBefore(moment()) ? "#DAD8D8" : "black"};
 `;
 
 const format = 'YYYY-MM-DD';
@@ -46,8 +86,7 @@ const Calendar: React.FC<calendarProps> = ({
     cssType = "001"
 }: any) => {
     // const [dates, setDates] = useState(getDates({startDate: moment().startOf('month').format(format), endDate: moment().add(pageSize, 'months').endOf('month').format(format)}));
-    const [dates, setDates] = useState(getDatesEachMonths({baseDate: moment().format(format), monthPageSize: pageSize}));
-    console.log('dates', JSON.stringify(dates));
+    const [dates, setDates] = useState(getDatesEachMonths({ baseDate: moment().format(format), monthPageSize: pageSize }));
     const Calendar001 = useMemo(() => {
         return (
             <Container>
@@ -60,7 +99,115 @@ const Calendar: React.FC<calendarProps> = ({
                     <Container3><Text1>금</Text1></Container3>
                     <Container3><Text1>토</Text1></Container3>
                 </Container2>
-                <Container4></Container4>
+                <Container4>
+                    {
+                        dates.map((date, index) => {
+                            const dayIndex = moment(date[0]).day();
+                            let dateIndex = 0;
+                            return (
+                                <Container5 key={index}>
+                                    <Container6>
+                                        <Text2>{moment(date[0]).format('M월 YYYY')}</Text2>
+                                    </Container6>
+                                    <Container7>
+                                        {
+                                            _.map(_.range(0, 7), index => {
+                                                if (index < dayIndex) {
+                                                    return (
+                                                        <Container8></Container8>
+                                                    );
+                                                } else {
+                                                    return (
+                                                        <Container8>
+                                                            <Container9>
+                                                                <Text3 date={date[dateIndex]}>{moment(date[dateIndex++]).format('D')}</Text3>
+                                                            </Container9>
+                                                        </Container8>
+                                                    );
+                                                }
+                                            })
+                                        }
+                                    </Container7>
+                                    <Container7>
+                                        {
+                                            _.map(_.range(0, 7), index => {
+                                                return (
+                                                    <Container8>
+                                                        <Container9>
+                                                            <Text3 date={date[dateIndex]}>{moment(date[dateIndex++]).format('D')}</Text3>
+                                                        </Container9>
+                                                    </Container8>
+                                                );
+                                            })
+                                        }
+                                    </Container7>
+                                    <Container7>
+                                        {
+                                            _.map(_.range(0, 7), index => {
+                                                return (
+                                                    <Container8>
+                                                        <Container9>
+                                                            <Text3 date={date[dateIndex]}>{moment(date[dateIndex++]).format('D')}</Text3>
+                                                        </Container9>
+                                                    </Container8>
+                                                );
+                                            })
+                                        }
+                                    </Container7>
+                                    <Container7>
+                                        {
+                                            _.map(_.range(0, 7), index => {
+                                                return (
+                                                    <Container8>
+                                                        <Container9>
+                                                            <Text3 date={date[dateIndex]}>{moment(date[dateIndex++]).format('D')}</Text3>
+                                                        </Container9>
+                                                    </Container8>
+                                                );
+                                            })
+                                        }
+                                    </Container7>
+                                    <Container7>
+                                        {
+                                            _.map(_.range(0, 7), index => {
+                                                if (_.gte(dateIndex, _.size(date))) {
+                                                    return (
+                                                        <Container8></Container8>
+                                                    );
+                                                }
+                                                return (
+                                                    <Container8>
+                                                        <Container9>
+                                                            <Text3 date={date[dateIndex]}>{moment(date[dateIndex++]).format('D')}</Text3>
+                                                        </Container9>
+                                                    </Container8>
+                                                );
+                                            })
+                                        }
+                                    </Container7>
+                                    <Container7>
+                                        {
+                                            _.map(_.range(0, 7), index => {
+                                                if (_.gte(dateIndex, _.size(date))) {
+                                                    return (
+                                                        <Container8></Container8>
+                                                    );
+                                                }
+                                                return (
+                                                    <Container8>
+                                                        <Container9>
+                                                            <Text3 date={date[dateIndex]}>{moment(date[dateIndex++]).format('D')}</Text3>
+                                                        </Container9>
+                                                    </Container8>
+                                                );
+                                            })
+                                        }
+                                    </Container7>
+                                </Container5>
+                            )
+                        })
+                    }
+                </Container4>
             </Container>
         );
     }, []);
